1c505de980cb711c626a9773d1240c96ab63ecfd
Albert Morgese <fusxfaranto@gmail.com>
2020-02-08 14:06:37 -0800

    it begins

---
+++ web/src/index.js
@@ -0,0 +1,231 @@
+import React, { useRef, useEffect, useState } from 'react';
+//import Konva from 'konva';
+import { render } from 'react-dom';
+import { Circle, Stage, Layer, Line } from 'react-konva';
+
+import './index.css';
+
+
+Math.clamp = function(number, min, max) {
+    return Math.max(min, Math.min(number, max));
+};
+
+
+
+const Foo = props => {
+    const [state, setState] = useState({
+        isDragging: false,
+        x: 50,
+        y: 50
+    });
+
+    return (
+        <Circle
+          x={state.x}
+          y={state.y}
+          radius={10}
+          draggable
+          fill={state.isDragging ? 'green' : 'red'}
+          onDragStart={() => {
+              setState({
+                  isDragging: true
+              });
+          }}
+          onDragEnd={e => {
+              console.log(e.target.x(), e.target.y());
+              setState({
+                  isDragging: false,
+                  x: e.target.x(),
+                  y: e.target.y()
+              });
+          }}
+        /* onDragMove={e => { */
+        /*     //console.log(e); */
+        /* }} */
+          dragBoundFunc={pos => {
+              //console.log(pos);
+              return {
+                  x: Math.clamp(pos.x, 0, props.stageWidth),
+                  y: Math.clamp(pos.y, 0, props.stageHeight),
+              };
+          }}
+        />);
+};
+
+const GridDir = Object.freeze({
+    v: Symbol("GridDir.v"),
+    h: Symbol("GridDir.h"),
+});
+
+const Grid = props => {
+    let elems = [];
+    let to = (() => {
+        switch (props.dir) {
+        case GridDir.v:
+            return props.width;
+        case GridDir.h:
+            return props.height;
+        }
+        return null;
+    })();
+    for (let i = 0; i < to; i += props.span) {
+        let points = (() => {
+            switch (props.dir) {
+            case GridDir.v:
+                return [i, 0, i, props.height];
+            case GridDir.h:
+                return [0, i, props.width, i];
+            }
+            return null;
+        })();
+        elems.push(<Line
+                     points={points}
+                     stroke='gray'
+                     strokeWidth={1}
+                   />);
+    }
+
+    return (
+        <>
+          {elems}
+        </>
+    );
+};
+
+const Chart = props => {
+    const ref = useRef();
+    const [dim, setDim] = useState({
+        width: 0,
+        height: 0,
+    });
+    const [skipStage, setSkipStage] = useState(true);
+
+    const autoSetDim = () => {
+        if (ref.current) {
+            setDim({
+                width: ref.current.offsetWidth,
+                height: ref.current.offsetHeight,
+            });
+        }
+    };
+
+    useEffect(() => {
+        autoSetDim();
+        setSkipStage(false);
+    }, []);
+
+    useEffect(() => {
+        let resize_timer = null;
+        const handler = () => {
+            clearInterval(resize_timer);
+            autoSetDim();
+            setSkipStage(true);
+            resize_timer = setTimeout(() => { setSkipStage(false); }, 200);
+        };
+
+        window.addEventListener('resize', handler);
+
+        return () => window.removeEventListener('resize', handler);
+    }, []);
+
+    const stageW = dim.width;// - 100;
+    const stageH = dim.height;// - 100;
+
+    return (
+        <div
+          className="chart-container"
+          ref={ref}
+        >
+          { !skipStage &&
+            <Stage width={stageW} height={stageH}>
+              <Layer>
+                <Grid
+                  width={stageW}
+                  height={stageH}
+                  dir={GridDir.v}
+                  span={50}
+                />
+                <Grid
+                  width={stageW}
+                  height={stageH}
+                  dir={GridDir.h}
+                  span={50}
+                />
+                <Foo
+                  stageWidth={stageW}
+                  stageHeight={stageH}
+                />
+              </Layer>
+            </Stage>
+          }
+        </div>
+    );
+};
+
+
+const ProgMenuEntry = props => {
+    const [hideVars, setHideVars] = useState(false);
+
+    const arrow = hideVars ? '>' : 'V';
+
+    const prog = props.prog;
+
+    const varsList = hideVars ? null :
+
+          <ul className="prog-menu-list">
+            {prog.locals.map((local =>
+                              <li key={local}> {local} </li>
+                             ))}
+          </ul>;
+    
+    return (
+        <>
+          <li>
+            <div onClick={() => setHideVars(!hideVars)}>
+              {arrow}
+              &nbsp;
+              {prog.name}
+            </div>
+
+            {varsList}
+          </li>
+        </>
+    );
+};
+
+const ProgMenu = props => {
+    return (
+        <ul className="prog-menu-list">
+          {props.progs.map((prog => <ProgMenuEntry prog={prog} key={prog.name}/>))}
+        </ul>
+    );
+};
+
+const App = props => {
+    const progs = [
+        {
+            name: "testo",
+            locals: ["pitch", "volume"],
+            prog: "",
+        },
+        {
+            name: "testo2",
+            locals: ["pitch", "volume"],
+            prog: "",
+        },
+    ];
+
+    return (
+        <div className="app-container">
+          <div className="menu-container">
+            <ProgMenu progs={progs} />
+          </div>
+          <div className="app-side-container">
+            <div className="other-container"></div>
+            <Chart />
+          </div>
+        </div>
+    );
+};
+
+render(<App />, document.getElementById('root'));
